cbbf69d0ac9b76d3fdb28ec8d65725b8e37e1659
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2015-01-17 13:04:21 +0100

    bug fix

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@ if [ "${ROOTSYS}" = "" ]; then
   echo "!!!! Set ROOT environment; Abort." 
 else
   echo "ROOT is ready :-)"
-  if [ "${DONE}" = "" ]; then
+  if [ "${BASEDIR}" != "${PWD}" ]; then
     export BASEDIR=${PWD}
     export CPATH=${BASEDIR}/include
     export LD_LIBRARY_PATH=${BASEDIR}/lib:${LD_LIBRARY_PATH}
     export DYLD_LIBRARY_PATH=${LD_LIBRARY_PATH}
-    export DONE=1
   else
     echo "The environment is already set."
   fi
