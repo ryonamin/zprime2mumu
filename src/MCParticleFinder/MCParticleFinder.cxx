31f9df9acebd3a6c1e22f236b369a98e0358ae30
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2015-02-27 18:26:52 +0100

    fixed a bug that is related to phi matching

--- src/MCParticleFinder/MCParticleFinder.cxx
+++ src/MCParticleFinder/MCParticleFinder.cxx
@@ -1,6 +1,8 @@
 #include "MCParticleFinder.h"
 #include "TreeHandler.h"
+#include "TMath.h"
 #include <iostream>
+#include <algorithm>
 
 ClassImp(MCParticleFinder)
 
@@ -20,11 +22,16 @@ int MCParticleFinder::getMatchedMCId(int recoId)
   for ( int ip = 0; ip < fT.mc_pt->size(); ip++ ) {
     if ( (*fT.mc_status)[ip] != 1 ) continue; // only check the final state particles
     if ( TMath::Abs((*fT.mc_pdgId)[ip]) != 13 ) continue;
-    if ( (*fT.mc_charge)[ip] != (*fT.mu_tevOptimized_charge)[recoId] ) continue;
+    //if ( (*fT.mc_charge)[ip] != (*fT.mu_tevOptimized_charge)[recoId] ) continue;
     if ( (*fT.mc_pt)[ip] < (*fT.mu_tevOptimized_pt)[recoId] - fnsig_pt*(*fT.mu_tevOptimized_ptError)[recoId] ) continue;
     if ( (*fT.mc_pt)[ip] > (*fT.mu_tevOptimized_pt)[recoId] + fnsig_pt*(*fT.mu_tevOptimized_ptError)[recoId] ) continue;
-    if ( (*fT.mc_phi)[ip] < (*fT.mu_tevOptimized_phi)[recoId] - fnsig_phi*(*fT.mu_tevOptimized_phiError)[recoId] ) continue;
-    if ( (*fT.mc_phi)[ip] > (*fT.mu_tevOptimized_phi)[recoId] + fnsig_phi*(*fT.mu_tevOptimized_phiError)[recoId] ) continue;
+
+    // Take periodic boundary into account
+    float dphi = TMath::Abs( fT.mc_phi->at(ip) - fT.mu_tevOptimized_phi->at(recoId) );
+    // Take smaller central angle
+    dphi = std::min( dphi, float(2*TMath::Pi()-dphi) );
+    if ( dphi > fnsig_phi*fT.mu_tevOptimized_phiError->at(recoId) ) continue;
+
     if ( (*fT.mc_eta)[ip] < (*fT.mu_tevOptimized_eta)[recoId] - fnsig_eta*(*fT.mu_tevOptimized_etaError)[recoId] ) continue;
     if ( (*fT.mc_eta)[ip] > (*fT.mu_tevOptimized_eta)[recoId] + fnsig_eta*(*fT.mu_tevOptimized_etaError)[recoId] ) continue;
     nMatched.push_back(ip);
