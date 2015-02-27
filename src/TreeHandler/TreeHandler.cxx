62e1abc31266cccf14054a1e94fe9165ae37def9
Ryo Yonamine <ryo.yonamine@ulb.ac.be>
2015-02-27 12:24:44 +0100

    Added some tree variables

--- src/TreeHandler/TreeHandler.cxx
+++ src/TreeHandler/TreeHandler.cxx
@@ -16,12 +16,31 @@ TreeHandler::TreeHandler(std::string treename,
 	                                                 mc_pdgId(0),
 	                                                 mc_charge(0),
 	                                                 mc_status(0),
+                                                         mu_isolationR03_sumPt(0),
+							 mu_isGlobalMuon(0),
+                                                         mu_isStandAloneMuon(0),
+                                                         mu_isTrackerMuon(0),
+                                                         mu_isPFMuon(0),
+                                                         mu_isPFIsolationValid(0),
+                                                         mu_numberOfMatchedStations(0),
+                                                         mu_numberOfValidPixelHits(0),
 			                                 mu_gt_pt(0),
 			                                 mu_gt_ptError(0),
 			                                 mu_gt_phi(0),
 			                                 mu_gt_phiError(0),
-			                                 mu_gt_theta(0),
-			                                 mu_gt_thetaError(0),
+			                                 mu_gt_eta(0),
+			                                 mu_gt_etaError(0),
+			                                 mu_gt_charge(0),
+			                                 mu_gt_px(0),
+			                                 mu_gt_py(0),
+			                                 mu_gt_pz(0),
+                                                         mu_gt_dz(0),
+                                                         mu_gt_dz_beamspot(0),
+                                                         mu_gt_dz_firstPVtx(0),
+                                                         mu_gt_dxy(0),
+                                                         mu_gt_dxy_beamspot(0),
+                                                         mu_gt_dxy_firstPVtx(0),
+                                                         mu_gt_normalizedChi2(0),
 			                                 mu_tevOptimized_pt(0),
 			                                 mu_tevOptimized_ptError(0),
 			                                 mu_tevOptimized_phi(0),
@@ -31,37 +50,73 @@ TreeHandler::TreeHandler(std::string treename,
 			                                 mu_tevOptimized_charge(0),
 			                                 mu_tevOptimized_px(0),
 			                                 mu_tevOptimized_py(0),
-			                                 mu_tevOptimized_pz(0)
+			                                 mu_tevOptimized_pz(0),
+                                                         mu_tevOptimized_dz(0),
+                                                         mu_tevOptimized_dz_beamspot(0),
+                                                         mu_tevOptimized_dz_firstPVtx(0),
+                                                         mu_tevOptimized_dxy(0),
+                                                         mu_tevOptimized_dxy_beamspot(0),
+                                                         mu_tevOptimized_dxy_firstPVtx(0)
 {
   for (unsigned int i = 0; i < flist.size(); i++ ) {
     Add(flist[i].c_str());
   }
-  SetBranchAddress("mc_pt",&mc_pt);
-  SetBranchAddress("mc_phi",&mc_phi);
-  SetBranchAddress("mc_eta",&mc_eta);
-  SetBranchAddress("mc_mass"           , &mc_mass) ;
-  SetBranchAddress("mc_px"             , &mc_px) ;
-  SetBranchAddress("mc_py"             , &mc_py) ;
-  SetBranchAddress("mc_pz"             , &mc_pz) ;
-  SetBranchAddress("mc_pdgId"          , &mc_pdgId) ;
-  SetBranchAddress("mc_charge"         , &mc_charge) ;
-  SetBranchAddress("mc_status"         , &mc_status) ;
-  SetBranchAddress("mu_gt_pt"          , &mu_gt_pt) ;
-  SetBranchAddress("mu_gt_ptError"     , &mu_gt_ptError) ;
-  SetBranchAddress("mu_gt_phi"         , &mu_gt_phi) ;
-  SetBranchAddress("mu_gt_phiError"    , &mu_gt_phiError) ;
-  SetBranchAddress("mu_gt_theta"       , &mu_gt_theta) ;
-  SetBranchAddress("mu_gt_thetaError"  , &mu_gt_thetaError) ;
-  SetBranchAddress("mu_tevOptimized_pt",&mu_tevOptimized_pt) ;
-  SetBranchAddress("mu_tevOptimized_ptError",&mu_tevOptimized_ptError) ;
-  SetBranchAddress("mu_tevOptimized_phi",&mu_tevOptimized_phi) ;
-  SetBranchAddress("mu_tevOptimized_phiError",&mu_tevOptimized_phiError) ;
-  SetBranchAddress("mu_tevOptimized_eta",&mu_tevOptimized_eta) ;
-  SetBranchAddress("mu_tevOptimized_etaError",&mu_tevOptimized_etaError) ;
-  SetBranchAddress("mu_tevOptimized_charge",&mu_tevOptimized_charge) ;
-  SetBranchAddress("mu_tevOptimized_px",&mu_tevOptimized_px) ;
-  SetBranchAddress("mu_tevOptimized_py",&mu_tevOptimized_py) ;
-  SetBranchAddress("mu_tevOptimized_pz",&mu_tevOptimized_pz) ;
+  // mc
+  SetBranchAddress("mc_pt"                         , &mc_pt);
+  SetBranchAddress("mc_phi"                        , &mc_phi);
+  SetBranchAddress("mc_eta"                        , &mc_eta);
+  SetBranchAddress("mc_mass"                       , &mc_mass) ;
+  SetBranchAddress("mc_px"                         , &mc_px) ;
+  SetBranchAddress("mc_py"                         , &mc_py) ;
+  SetBranchAddress("mc_pz"                         , &mc_pz) ;
+  SetBranchAddress("mc_pdgId"                      , &mc_pdgId) ;
+  SetBranchAddress("mc_charge"                     , &mc_charge) ;
+  SetBranchAddress("mc_status"                     , &mc_status) ;
+  // muon
+  SetBranchAddress("mu_isolationR03_sumPt"         , &mu_isolationR03_sumPt) ;
+  SetBranchAddress("mu_isGlobalMuon"               , &mu_isGlobalMuon           ) ; 
+  SetBranchAddress("mu_isStandAloneMuon"           , &mu_isStandAloneMuon       ) ;  
+  SetBranchAddress("mu_isTrackerMuon"              , &mu_isTrackerMuon          ) ; 
+  SetBranchAddress("mu_isPFMuon"                   , &mu_isPFMuon               ) ;  
+  SetBranchAddress("mu_isPFIsolationValid"         , &mu_isPFIsolationValid     ) ;      
+  SetBranchAddress("mu_numberOfMatchedStations"    , &mu_numberOfMatchedStations) ; 
+  SetBranchAddress("mu_numberOfValidPixelHits"     , &mu_numberOfValidPixelHits ) ; 
+  // global muon
+  SetBranchAddress("mu_gt_pt"                      , &mu_gt_pt) ;
+  SetBranchAddress("mu_gt_ptError"                 , &mu_gt_ptError) ;
+  SetBranchAddress("mu_gt_phi"                     , &mu_gt_phi) ;
+  SetBranchAddress("mu_gt_phiError"                , &mu_gt_phiError) ;
+  SetBranchAddress("mu_gt_eta"                     , &mu_gt_eta) ;
+  SetBranchAddress("mu_gt_etaError"                , &mu_gt_etaError) ;
+  SetBranchAddress("mu_gt_charge"                  , &mu_gt_charge) ;
+  SetBranchAddress("mu_gt_px"                      , &mu_gt_px) ;
+  SetBranchAddress("mu_gt_py"                      , &mu_gt_py) ;
+  SetBranchAddress("mu_gt_pz"                      , &mu_gt_pz) ;
+  SetBranchAddress("mu_gt_dz"                      , &mu_gt_dz            ) ;  
+  SetBranchAddress("mu_gt_dz_beamspot"             , &mu_gt_dz_beamspot   ) ;   
+  SetBranchAddress("mu_gt_dz_firstPVtx"            , &mu_gt_dz_firstPVtx  ) ; 
+  SetBranchAddress("mu_gt_dxy"                     , &mu_gt_dxy           ) ;  
+  SetBranchAddress("mu_gt_dxy_beamspot"            , &mu_gt_dxy_beamspot  ) ; 
+  SetBranchAddress("mu_gt_dxy_firstPVtx"           , &mu_gt_dxy_firstPVtx ) ; 
+
+  SetBranchAddress("mu_gt_normalizedChi2"          , &mu_gt_normalizedChi2) ; 
+  // tev optimized muon
+  SetBranchAddress("mu_tevOptimized_pt"            , &mu_tevOptimized_pt) ;
+  SetBranchAddress("mu_tevOptimized_ptError"       , &mu_tevOptimized_ptError) ;
+  SetBranchAddress("mu_tevOptimized_phi"           , &mu_tevOptimized_phi) ;
+  SetBranchAddress("mu_tevOptimized_phiError"      , &mu_tevOptimized_phiError) ;
+  SetBranchAddress("mu_tevOptimized_eta"           , &mu_tevOptimized_eta) ;
+  SetBranchAddress("mu_tevOptimized_etaError"      , &mu_tevOptimized_etaError) ;
+  SetBranchAddress("mu_tevOptimized_charge"        , &mu_tevOptimized_charge) ;
+  SetBranchAddress("mu_tevOptimized_px"            , &mu_tevOptimized_px) ;
+  SetBranchAddress("mu_tevOptimized_py"            , &mu_tevOptimized_py) ;
+  SetBranchAddress("mu_tevOptimized_pz"            , &mu_tevOptimized_pz) ;
+  SetBranchAddress("mu_tevOptimized_dz"            , &mu_tevOptimized_dz            ) ;  
+  SetBranchAddress("mu_tevOptimized_dz_beamspot"   , &mu_tevOptimized_dz_beamspot   ) ;   
+  SetBranchAddress("mu_tevOptimized_dz_firstPVtx"  , &mu_tevOptimized_dz_firstPVtx  ) ; 
+  SetBranchAddress("mu_tevOptimized_dxy"           , &mu_tevOptimized_dxy           ) ;  
+  SetBranchAddress("mu_tevOptimized_dxy_beamspot"  , &mu_tevOptimized_dxy_beamspot  ) ; 
+  SetBranchAddress("mu_tevOptimized_dxy_firstPVtx" , &mu_tevOptimized_dxy_firstPVtx ) ; 
 }
 
 void TreeHandler::DumpInputs() const {
--- src/TreeHandler/TreeHandler.h
+++ src/TreeHandler/TreeHandler.h
@@ -19,12 +19,38 @@ class TreeHandler : public TChain
   std::vector<int>* mc_pdgId;                 
   std::vector<int>* mc_charge;
   std::vector<int>* mc_status;
+
+  // muon
+  std::vector<float>* mu_isolationR03_sumPt;
+  std::vector<bool>*  mu_isGlobalMuon;
+  std::vector<bool>*  mu_isStandAloneMuon;
+  std::vector<bool>*  mu_isTrackerMuon;
+  std::vector<bool>*  mu_isPFMuon;
+  std::vector<bool>*  mu_isPFIsolationValid;
+  std::vector<int>*   mu_numberOfMatchedStations;
+  std::vector<int>*   mu_numberOfValidPixelHits;
+
+  // global muon
   std::vector<float>* mu_gt_pt;
   std::vector<float>* mu_gt_ptError;
   std::vector<float>* mu_gt_phi;
   std::vector<float>* mu_gt_phiError;
-  std::vector<float>* mu_gt_theta;
-  std::vector<float>* mu_gt_thetaError;
+  std::vector<float>* mu_gt_eta;
+  std::vector<float>* mu_gt_etaError;
+  std::vector<int>*   mu_gt_charge;
+  std::vector<float>* mu_gt_px;
+  std::vector<float>* mu_gt_py;           
+  std::vector<float>* mu_gt_pz;           
+  std::vector<float>* mu_gt_dz;
+  std::vector<float>* mu_gt_dz_beamspot;
+  std::vector<float>* mu_gt_dz_firstPVtx;
+  std::vector<float>* mu_gt_dxy;
+  std::vector<float>* mu_gt_dxy_beamspot;
+  std::vector<float>* mu_gt_dxy_firstPVtx;
+
+  std::vector<float>* mu_gt_normalizedChi2;
+
+  // tev optimized muon
   std::vector<float>* mu_tevOptimized_pt;
   std::vector<float>* mu_tevOptimized_ptError;
   std::vector<float>* mu_tevOptimized_phi;
@@ -35,6 +61,12 @@ class TreeHandler : public TChain
   std::vector<float>* mu_tevOptimized_px;
   std::vector<float>* mu_tevOptimized_py;           
   std::vector<float>* mu_tevOptimized_pz;           
+  std::vector<float>* mu_tevOptimized_dz;
+  std::vector<float>* mu_tevOptimized_dz_beamspot;
+  std::vector<float>* mu_tevOptimized_dz_firstPVtx;
+  std::vector<float>* mu_tevOptimized_dxy;
+  std::vector<float>* mu_tevOptimized_dxy_beamspot;
+  std::vector<float>* mu_tevOptimized_dxy_firstPVtx;
   
   void DumpInputs() const;
   private :
